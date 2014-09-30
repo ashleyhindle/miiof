d3628b28ac2a538fac3c43c477b41a8b55180501
Ashley Hindle <ashley@smellynose.com>
2014-09-30 18:55:09 +0000

    you can now add a new invoice item, woo

--- web/assets/js/miiof.js
+++ web/assets/js/miiof.js
@@ -5,8 +5,9 @@ var miiofApp = angular.module('miiofApp', []).config(function($interpolateProvid
 
 miiofApp.controller('CreateCtrl', function ($scope) {
 		$scope.lastInvoiceId = 1000;
-		$scope.exampleInvoice = {
+		$scope.invoice = {
 				'invoiceId': 1000,
+				'date': moment().format('MMMM Do YYYY'),
 				'notes': "Bank details go here, maybe payment terms",
 				'currency': {
 						'code': 'GBP',
@@ -26,24 +27,34 @@ miiofApp.controller('CreateCtrl', function ($scope) {
 						'phone': '+1 9120 621 XX',
 						'address': 'Idontknow American Addresses, 42nd Street, LA'
 				},
-				'items': {
-						0: {
+				'items': [
+						{
 								'itemId' : 920000,
 								'description': 'Red Wine',
 								'quantity': 3,
 								'price': 32
 						},
-						1: {
+						{
 								'itemId': 920001,
 								'description': 'White Wine In The Sun',
 								'quantity': 82,
 								'price': 10.80
 						}
-				},
+				],
+				'lastItemId': 92001,
 				'total': (3 * 32) + (82 * 10.80)
 		}
 
 		$scope.invoices = {
 		
 		};
+
+		$scope.addItem = function() {
+				$scope.invoice['items'].push({
+						'itemId': $scope.lastItemId + 1,
+						'description': '',
+						'quantity': 0,
+						'price': 0.00
+				});
+		}
 });
