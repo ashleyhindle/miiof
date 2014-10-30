4c54a839513350fd4bd1cf6dd0efcd4bf1cdb20e
Ashley Hindle <ashley@smellynose.com>
2014-10-30 13:12:59 +0000

    When saving to dropbox, set the file name betterer

--- src/Miiof/Controller/DropboxController.php
+++ src/Miiof/Controller/DropboxController.php
@@ -38,16 +38,17 @@ class DropboxController extends Controller
 		}
 
 		$invoiceKey = $request->get('invoiceKey');
+  $invoice = json_decode($app['predis']->get('invoice:'.$invoiceKey), true);
 
 		$dbxClient = new dbx\Client($app['session']->get('dropbox')['accessToken'], "Miiof");
-		$fp = fopen('/tmp/' . $invoiceKey . '/INVOICE_' . $invoiceKey . '.pdf', 'rb');
-		$md1 = $dbxClient->uploadFile("/INVOICE_{$invoiceKey}.pdf", dbx\WriteMode::add(), $fp);
+		$fp = fopen('/tmp/' . $invoiceKey . '/INVOICE_' . $invoice['invoiceid'] . '.pdf', 'rb');
+		$md1 = $dbxClient->uploadFile("/INVOICE_{$invoice['invoiceid']}.pdf", dbx\WriteMode::add(), $fp);
 		fclose($fp);
 
 		if(is_array($md1) && $md1['revision']) {
-				$app['session']->getFlashBag()->add('success', "Successfully uploaded INVOICE_{$invoiceKey}.pdf");
+				$app['session']->getFlashBag()->add('success', "Successfully uploaded INVOICE_{$invoice['invoiceid']}.pdf");
 		} else {
-				$app['session']->getFlashBag()->add('warning', "Failed to upload INVOICE_{$invoiceKey}.pdf - I don't know why right now, but I am very sorry! :-(");
+				$app['session']->getFlashBag()->add('warning', "Failed to upload INVOICE_{$invoice['invoiceid']}.pdf - I don't know why right now, but I am very sorry! :-(");
 		}
 		return $app->redirect('/save/'.$invoiceKey);
 	}
